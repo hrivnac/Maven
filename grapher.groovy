806f447741645cabe3e04fffcfb2cdbf4608969c
Julius Hrivnac <julius.hrivnac@cern.ch>
2025-05-20 15:03:10 +0200

    ...

--- grapher.groovy
+++ grapher.groovy
@@ -1,8 +1,8 @@
 @GrabResolver(name='hrivnac', root='https://raw.githubusercontent.com/hrivnac/Maven/main/')
 @GrabResolver(name='central', root='https://repo1.maven.org/maven2/')
-@Grab(group='com.hrivnac',              module='Grapher', version='01.02.00')
-@Grab(group='com.hrivnac',              module='Lomikel', version='03.06.00')
-@Grab(group='com.hrivnac',              module='Lomikel', version='03.06.00-ext')
+@Grab(group='com.hrivnac',              module='Grapher',    version='01.02.00')
+@Grab(group='com.hrivnac',              module='Lomikel',    version='03.06.00')
+@Grab(group='com.hrivnac',              module='Lomikel',    version='03.06.00-ext')
 @Grab(group='org.apache.logging.log4j', module='log4j-core', version='2.23.1')
 
 import com.Lomikel.Apps.LUC
